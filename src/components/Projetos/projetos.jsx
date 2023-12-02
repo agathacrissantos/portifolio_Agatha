4032d019304864dd83ff62ed86f02fc996828fb0
Agatha <agathacrissantos6@gmail.com>
2023-12-01 22:36:36 -0300

    atualizações em projetos

--- src/components/Projetos/projetos.jsx
+++ src/components/Projetos/projetos.jsx
@@ -1,12 +1,30 @@
 import * as S from "./projetos.style.jsx"
+import Fantastika from "../../assets/Fantastika.png"
+import Calculadora from "../../assets/Calculadora.png"
 
-export default function Projetos(){
+export default function Projetos() {
 
     return (
         <>
-        <S.MeusProjetos>
-        <S.Titulo>Conheça um pouco dos meus Projetos de desenvolvimento Front-end</S.Titulo>
-        </S.MeusProjetos>
+            <S.MeusProjetos>
+                <S.Titulo>Conheça um pouco dos meus Projetos de desenvolvimento Front-end</S.Titulo>
+                <S.Container>
+                    <S.Figure>
+                        <a href="https://agathacrissantos.github.io/Fantastika-Desafio_final/" target="_blank" >
+                            <S.Imagem src={Fantastika} alt="Projeto Página Fantastika" />
+                        </a>
+                        <h4>Esse desafio foi desenvolvido para fins didáticos, utilizando HTML e CSS,para construção da página e @media para a responsividade. Clique na imagem e acesse o deploy ou clique no link do meu github no início.</h4>
+                    </S.Figure>
+
+                    <S.Figure>
+                        <a href="https://desafio-react-calculadora.vercel.app/" target="_blank">
+                            <S.Imagem src={Calculadora} alt="Projeto calculadora" />
+                        </a>
+                        <h4>Esse projeto foi desenvolvido com ReactJs,styled-components,@media, funções e muita lógica de programação. Clique na imagem e acesse o deploy ou clique no link do meu github no início.</h4>
+                    </S.Figure>
+                </S.Container>
+
+            </S.MeusProjetos>
         </>
     )
 }
--- src/components/Projetos/projetos.style.jsx
+++ src/components/Projetos/projetos.style.jsx
@@ -12,3 +12,27 @@ justify-content:center;
 font-size:30px;
 font-family: Dancing Script, serif;
 `
+
+export const Container = styled.div`
+display: flex;
+justify-content: space-around;
+align-items: center;
+flex-wrap: wrap;
+padding-top: 40px;
+`
+
+export const Figure = styled.figure`
+width: 30vw;
+display: flex;
+h4{
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+    line-height: 25px;
+}
+`
+
+export const Imagem = styled.img`
+height: 36vh;
+width: 17vw;
+`
